406f3fd059751311cde33d45b21b9f7ab95813ed
Jason <jchoy@outlook.com>
2025-12-04 20:42:36 -0800

    preliminary skeleton, waiting on auth

--- we-explore-earth/frontend/app/(users)/profile/index.tsx
+++ we-explore-earth/frontend/app/(users)/profile/index.tsx
@@ -1,13 +1,50 @@
+//STANDARD LIBRARY
+import React from 'react';
 import { View, Text } from 'react-native';
+import { useState, useEffect} from 'react';
+
+//LOCAL FILES
+import { styles } from './styles';
+import { User } from "../../../types/user";
+
 
 export default function ProfileScreen() {
+  //REACT HOOKS
+  const { userId } = useUser();
+  //STATE VARIABLES
+  const [user, setUser] = useState<User | null>(null);
+  //HANDLERS
+  const fetchUser = async (userId: string) => {
+    try {
+      const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/users/${userId}`, {
+        method: 'GET', 
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to fetch user');
+      }
+
+      const userData: User = await response.json();
+      setUser(userData);
+    } catch(error: any) {
+      console.error('Error while fetching user:', error);
+    }
+  }
+  //EFFECTS
+  useEffect(() => {
+    if (userId) {
+      fetchUser(userId);
+    }
+  }, [userId]);
+
+
+  //RENDER
   return (
-    <View style={{ 
-      flex: 1, 
-      justifyContent: "center", 
-      alignItems: "center" 
-    }}>
-      <Text>Profile Page</Text>
+    <View style = {styles.container}>
+      <Text> Hi @{user?.username}!</Text>
     </View>
   );
 }
---
+++ we-explore-earth/frontend/app/(users)/profile/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  // All your different styles go in here
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
