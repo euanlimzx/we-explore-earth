498dc67a880612668f3e8247a5b4e4f7bc4b1806
aruushikaur <aruushi.kaur@gmail.com>
2026-01-08 21:24:32 -0800

    Remove .history files from repo

--- .history/we-explore-earth/frontend/app/(admin)/home/index_20260105214150.tsx
+++
@@ -1,9 +0,0 @@
-return (
-    <View style={{ 
-      flex: 1, 
-      justifyContent: "center", 
-      alignItems: "center" 
-    }}>
-      <Text>Home Page HERE</Text>
-    </View>
-  );
--- .history/we-explore-earth/frontend/app/(admin)/home/index_20260105214159.tsx
+++
@@ -1,9 +0,0 @@
-return (
-    <View style={{ 
-      flex: 1, 
-      justifyContent: "center", 
-      alignItems: "center" 
-    }}>
-      <Text>Welcome to Admin Home!</Text>
-    </View>
-  );
