c07e732226efba5b649c2352d74499354d924bb4
kellyy8 <kellyyu2886@gmail.com>
2026-02-10 11:34:03 -0800

    Refactored code to use sets instead of dictionaries to track selected options.

--- we-explore-earth/frontend/app/components/Calendar/eventFilters/eventFilters.tsx
+++ we-explore-earth/frontend/app/components/Calendar/eventFilters/eventFilters.tsx
@@ -7,21 +7,16 @@ import { styles } from './styles'
 function FilterHeader(
     {
         header,
-        selectedOptions,
         setSelectedOptions,
     }
     :
     {
         header: string,
-        selectedOptions: Record<string, boolean>,
         setSelectedOptions: React.Dispatch<any>
     }
 ){
     const handleReset = () => {
-        const clearedSelections = Object.fromEntries(
-            Object.entries(selectedOptions).map(([key]) => [key, false])
-        );
-        setSelectedOptions(clearedSelections);
+        setSelectedOptions(new Set());
     }
 
     return(
@@ -43,18 +38,21 @@ function FilterOption(
     :
     {
         option: string,
-        selectedOptions: Record<string, boolean>,
+        selectedOptions: Set<string>,
         setSelectedOptions: React.Dispatch<any>
     }
 ){
     const handleCheck = () => {
+        const updated = new Set(selectedOptions);
         // handle unchecking the box
-        if(selectedOptions[option]) {
-            setSelectedOptions({...selectedOptions, [option]: false});
+        if(selectedOptions.has(option)) {
+            updated.delete(option);
+            setSelectedOptions(updated);
         }
         // handle checking the box
         else {
-            setSelectedOptions({...selectedOptions, [option]: true});
+            updated.add(option);
+            setSelectedOptions(updated);
         }
     }
 
@@ -62,9 +60,9 @@ function FilterOption(
         <View style={styles.filterOptionWrapper}>
             <Text style={styles.filterOption}>{option}</Text>
             <Checkbox
-                value={selectedOptions[option]}
+                value={selectedOptions.has(option)}
                 onValueChange={handleCheck}
-                color={selectedOptions[option] ? 'black' : 'mediumgrey'}
+                color={selectedOptions.has(option) ? 'black' : 'mediumgrey'}
             />
         </View>
     )
@@ -72,20 +70,18 @@ function FilterOption(
 
 function EventFilters() {
     const [dateOptions, _] = useState<Array<string>>(['Today', 'Tomorrow', 'This Week', 'This Month']);
-    const [selectedDates, setSelectedDates] = useState<Record<string, boolean>>({});
+    const [selectedDates, setSelectedDates] = useState<Set<string>>(new Set());
 
     const [categoryOptions, setCategoryOptions] = useState<Array<string>>([]);
-    const [selectedCategories, setSelectedCategories] = useState<Record<string, boolean>>({});
+    const [selectedCategories, setSelectedCategories] = useState<Set<string>>(new Set());
 
     const handleSubmit = () => {
         // Compute start and end dates.
         let startDate : Date | null = null;
         let endDate : Date | null = null;
         const today = new Date();
-        for (const [option, isSelected] of Object.entries(selectedDates)) {
-            if (!isSelected) {
-                continue;
-            }
+
+        selectedDates.forEach((option) => {
             let newStartDate : Date = new Date();
             let newEndDate : Date = new Date();
             switch (option) {
@@ -101,26 +97,31 @@ function EventFilters() {
                     newEndDate.setDate(today.getDate() + (6-day));  // End of the week (Saturday)
                     break;
                 case 'This Month':
-                    newStartDate.setDate(1);  // First day of the month
+                    newStartDate.setDate(1);            // First day of the month
                     const numberOfDays = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
-                    newEndDate.setDate(numberOfDays); // Last day of the month
+                    newEndDate.setDate(numberOfDays);   // Last day of the month
                 default:
                     break;
             }
-            startDate = startDate ? new Date(Math.min(startDate.getTime(), newStartDate!.getTime())) : newStartDate;
-            endDate = endDate ? new Date(Math.max(endDate.getTime(), newEndDate!.getTime())) : newEndDate;
-        }
+            startDate = startDate ? new Date(Math.min(startDate.getTime(), newStartDate.getTime())) : newStartDate;
+            endDate = endDate ? new Date(Math.max(endDate.getTime(), newEndDate.getTime())) : newEndDate;
+        })
 
-        console.log('start date', startDate?.toString());
-        console.log('end date:', endDate?.toString());
-        const cats = [];
-        for (const [option, isSelected] of Object.entries(selectedCategories)) {
-            if (isSelected){
-                cats.push(option);
-            }
+        // TODO: Pass the filter values (startDate, endDate, categories) to parent component. Placeholder below.
+        console.log('');
+        if(startDate && endDate) {
+            console.log('start date', (startDate as Date).toString());
+            console.log('end date:', (endDate as Date).toString());
         }
-        console.log(cats);
-        // TODO: Pass the filter values (startDate, endDate, categories) to parent component.
+        else {
+            console.log('No dates selected.');
+        }
+        if(selectedCategories && selectedCategories.size) {
+            console.log(selectedCategories);
+        }
+        else {
+            console.log('No categories selected.');
+        }
     }
 
     async function retrieveCategories() {
@@ -139,23 +140,8 @@ function EventFilters() {
         }
     }
 
-    useEffect(() => {
-        // initially, all date options are unselected
-        const initSelectedDates : Record<string, boolean> = {};
-        dateOptions.forEach((date) => {
-            initSelectedDates[date] = false;
-        });
-        setSelectedDates(initSelectedDates);
-        
-        // grab categories from backend
-        retrieveCategories();
-
-        // initially, all category options are unselected
-        const initSelectedCategories : Record<string, boolean> = {};
-        categoryOptions.forEach((category) => {
-            initSelectedCategories[category] = false;
-        });
-        setSelectedCategories(initSelectedCategories);
+    useEffect(() => {        
+        retrieveCategories();  // grab categories from backend
     }, []);
 
     return(
@@ -166,7 +152,6 @@ function EventFilters() {
                     <>
                         <FilterHeader
                             header='Date'
-                            selectedOptions={selectedDates}
                             setSelectedOptions={setSelectedDates}
                         />
                         {dateOptions.map((option, index) => 
@@ -183,7 +168,6 @@ function EventFilters() {
                     <>
                         <FilterHeader
                             header='Category'
-                            selectedOptions={selectedCategories}
                             setSelectedOptions={setSelectedCategories}
                         />
                         {categoryOptions.map((option, index) => 
