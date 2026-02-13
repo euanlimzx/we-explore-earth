e809b16619a27e6a8163591353d41f571b601d1d
meenu-a <meenu@g.ucla.edu>
2026-02-12 21:43:47 -0800

    fixed folder layout and file names

---
+++ we-explore-earth/frontend/app/(onboarding)/notifications/index.tsx
@@ -0,0 +1,10 @@
+import OnboardingPage from '../components/onboardingPage';
+
+export default function NotificationsPage() {
+  return (
+    <OnboardingPage
+      title="Turn on Notifications"
+      nextRoute="/(users)/home"
+    />
+  );
+}
