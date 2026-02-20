879fa5b9d6b4f8cbc05e2558111b95c8a2e5e0f8
Vanshika Turkar <vanshika.turkar@gmail.com>
2026-02-19 20:55:12 -0800

    Debugging

--- we-explore-earth/backend/src/controllers/configController.ts
+++ we-explore-earth/backend/src/controllers/configController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import admin from "firebase-admin";
-import { updateDoc } from "firebase/firestore";
+// import { updateDoc } from "firebase/firestore";
 
 const db = admin.firestore();
 
